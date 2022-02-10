67d1d0494a28de820e72eeb7e989845e5f5e1e25
Sergiu78 <turutaserghei@mail.ru>
2022-02-10 19:47:01 +0200

    Delete Item from cart

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -14,7 +14,7 @@
                 <h3 class="box-title">Products Name</h3>
                 @if (Session::has('success_message'))
                     <div class="alert alert-success">
-                        <strong>Success</strong>{{ Session::get('success_message') }}
+                        <strong>Success </strong>{{ Session::get('success_message') }}
                     </div>
                 @endif
                 @if (Cart::count() > 0)
@@ -37,7 +37,7 @@
                         </div>
                         <div class="price-field sub-total"><p class="price">${{ $item->subtotal }}</p></div>
                         <div class="delete">
-                            <a href="#" class="btn btn-delete" title="">
+                            <a href="#" wire:click.prevent="destroy('{{ $item->rowId }}')" class="btn btn-delete" title="">
                                 <span>Delete from your cart</span>
                                 <i class="fa fa-times-circle" aria-hidden="true"></i>
                             </a>
@@ -67,7 +67,7 @@
                     <a class="link-to-shop" href="shop.html">Continue Shopping<i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
                 </div>
                 <div class="update-clear">
-                    <a class="btn btn-clear" href="#">Clear Shopping Cart</a>
+                    <a class="btn btn-clear" href="#" wire:click.prevent="destroyAll()">Clear Shopping Cart</a>
                     <a class="btn btn-update" href="#">Update Shopping Cart</a>
                 </div>
             </div>
